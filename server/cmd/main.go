ed5345339f107b353b349925705ed52ff8ce6608
Artem Lymarenko <artemchiklym@gmail.com>
2024-12-01 19:13:14 +0200

    * basic application logic

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,25 +2,11 @@ package main
 
 import (
 	"parallel-course-work/pkg/threadpool"
-	"sync/atomic"
-	"time"
+	"parallel-course-work/server/internal/app"
 )
 
 func main() {
-	pool := threadpool.New(10, 1)
-	pool.MustRun()
-
-	id := atomic.Int64{}
-	id.Store(0)
-
-	for range 100 {
-		task := threadpool.NewTask(id.Add(1), func() error {
-			time.Sleep(1 * time.Second)
-			return nil
-		})
-
-		pool.AddTask(task)
-	}
-	time.Sleep(6 * time.Second)
-	pool.MustTerminate()
+	tp := threadpool.New(4, 1)
+	server := app.New(8080, tp)
+	server.Start()
 }
